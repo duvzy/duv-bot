46d52d868f50257ae871007e45fca39eca99b3b0
duvzy <juanesjerez0101@gmail.com>
2026-03-09 23:20:15 -0500

    Add files via upload

---
+++ commands/s.js
@@ -0,0 +1,33 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    name: "s",
+
+    async execute(message, args, client) {
+
+        const snipes = client.snipes.get(message.channel.id);
+
+        if (!snipes || snipes.length ===0) {
+            return message.reply("No hay mensajes eliminados.");
+        }
+
+        const index = args[0] ? parseInt(args[0]) - 1 : 0;
+
+        if (!snipes[index]) {
+            return message.reply("⚠️ No hay mensajes eliminados guardados.");
+        }
+        
+        const embed = new EmbedBuilder ()
+            .setAuthor({ name: snipes.author, inconURL: snipe.avatar })
+            .setDescription(snipe.content)
+            .setColor("Red")
+            .setFooter({ text: `Snipe ${index + 1}` })
+            .setTimestamp(snipe.time);
+
+        if (snipe.image) embed.setImage(snipe.image);
+        if (snipe.sticker) embed.setImage(snipe.sticker);
+
+        message.channel.send({ embeds: [embed] });
+
+    }
+};
