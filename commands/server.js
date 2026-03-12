9d2ff9968bab9195c4aa9948e9425a51626815d8
duvzy <juanesjerez0101@gmail.com>
2026-03-12 02:52:02 -0500

    Add files via upload

---
+++ commands/server.js
@@ -0,0 +1,67 @@
+const { EmbedBuilder } = require/"discord.js";
+
+module.exports = {
+    name: "server",
+
+    async execute(message,args, client) {
+
+        const guild = message.guild;
+        
+        const owner = await guild.fetchOwner();
+
+        const embed = new EmbedBuilder()
+            .setColor("#2b2d31")
+            .setTitle(`📊 ${guild.name}`)
+            .setThumbnail(guild.iconURL({ dynamic: true, size: 1024 }))
+            .addFields(
+                {
+                    name: "👑 Dueño",
+                    value: `${owner.user.tag}`,
+                    inline: true
+                },
+                {
+                    name: "🆔 ID del servidor",
+                    value: guild.id,
+                    inline: true
+                },
+                {
+                    name: "📅 Creado",
+                    value: `${guild.memberCount}`,
+                    inline: true
+                },
+                {
+                    name: "👥 Miembros",
+                    value: `${guild.memberCount}`,
+                    inline: true
+                },
+                {
+                    name: "💬 Canales",
+                    value: `${guild.channels.cache.size}`,
+                    inline: true
+                },
+                {
+                    name: "🎭 Roles",
+                    value: `${guild.roles.cache.size}`,
+                    inline: true
+                },
+                {
+                    name: "😀 Emojis",
+                    value: `${guild.emojis.cache.size}`,
+                    inline: true
+                },
+                {
+                    name: "🚀 Boosts",
+                    value: `Nivel ${guild.premiumTier} (${guild.premiumSubscriptionCount || 0} boosts)`,
+                    inline: true
+                }
+            )
+            .setFooter({
+                text: `Pedido por ${message.author.tag}`,
+                iconURL: message.author.displayAvatarURL()
+            })
+            .setTimestamp();
+
+         message.channel.send({ embeds: [embed] });
+
+    }
+};
