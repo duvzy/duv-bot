51b37594e3264ea994a440735b256a474eb9971b
duvzy <juanesjerez0101@gmail.com>
2026-03-12 07:25:34 -0500

    hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,61 +1,90 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const fs = require("fs");
+const path = require("path");
 
 const gifs = [
-"https://c.tenor.com/nd_M3VFwVD0AAAAd/tenor.gif",
-"https://c.tenor.com/SYsRdiK-T7gAAAAd/tenor.gif",
-"https://c.tenor.com/k_aLQ7SgD04AAAAd/tenor.gif",
-"https://c.tenor.com/yMghDOetsPUAAAAC/tenor.gif",
-"https://c.tenor.com/FGb7ZIMzus8AAAAC/tenor.gif"
+    "https://c.tenor.com/nd_M3VFwVD0AAAAd/tenor.gif",
+    "https://c.tenor.com/SYsRdiK-T7gAAAAd/tenor.gif",
+    "https://c.tenor.com/k_aLQ7SgD04AAAAd/tenor.gif",
+    "https://c.tenor.com/yMghDOetsPUAAAAC/tenor.gif",
+    "https://c.tenor.com/FGb7ZIMzus8AAAAC/tenor.gif"
 ];
 
+const FILE = path.resolve(__dirname, "..", "hugs.json");
+
+function readData() {
+  try {
+    if (!fs.existsSync(FILE)) return {};
+    return JSON.parse(fs.readFileSync(FILE, "utf8") || "{}");
+  } catch (e) {
+    console.error("Error reading hugs.json:", e);
+    return {};
+  }
+}
+
+function writeData(data) {
+  try {
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 2), "utf8");
+  } catch (e) {
+    console.error("Error writing hugs.json:", e);
+  }
+}
+
 module.exports = {
-    name: "hug",
+  name: "hug",
 
-    async execute(message, args, client) {
+  async execute(message, args, client) {
+    // 1) Intentar mención -> cache -> fetch(ID)
+    let user = message.mentions.users.first();
 
-        const user =
-            message.mentions.users.first() ||
-            client.users.cache.get(args[0]);
-
-        if (!user) {
-            return message.reply("Menciona a alguien para abrazar 🤗");
+    if (!user && args[0]) {
+      // extraer sólo números (maneja <@!ID>, <@ID> o un ID puro)
+      const id = args[0].replace(/\D/g, "");
+      if (id.length === 17 || id.length === 18 || id.length === 19) {
+        try {
+          user = await client.users.fetch(id);
+        } catch (e) {
+          // no existe o no se pudo fetch
+          user = null;
         }
-
-        const path = "./hugs.json";
-
-        let data = {};
-
-        if (fs.existsSync(path)) {
-            data = JSON.parse(fs.readFileSync(path, "utf8"));
-        }
-
-        const key = `${message.author.id}_${user.id}`;
-
-        if (!data[key]) data[key] = 0;
-
-        data[key]++;
-
-        fs.writeFileSync(path, JSON.stringify(data, null, 2));
-
-        const gif = gifs[Math.floor(Math.random() * gifs.length)];
-
-        const embed = new EmbedBuilder()
-            .setColor("Pink")
-            .setDescription(`🤗 **${message.author.username}** ha abrazado a **${user.username}**!\n\n💞 Total abrazos: **${data[key]}**`)
-            .setImage(gif);
-
-        const button = new ButtonBuilder()
-            .setCustomId(`hugback_${message.author.id}`)
-            .setLabel("🤗 Abrazar de vuelta")
-            .setStyle(ButtonStyle.Primary);
-
-        const row = new ActionRowBuilder().addComponents(button);
-
-        message.channel.send({
-            embeds: [embed],
-            components: [row]
-        });
-
+      }
     }
+
+    if (!user) {
+      return message.reply("Menciona a alguien o proporciona su ID para abrazar 🤗");
+    }
+
+    if (user.id === message.author.id) {
+      return message.reply("No puedes abrazarte a ti mismo 🤨");
+    }
+
+    // 2) Leer/actualizar datos
+    const data = readData();
+
+    const key = `${message.author.id}_${user.id}`;
+    if (!data[key]) data[key] = 0;
+    data[key]++;
+
+    writeData(data);
+
+    // 3) Embed + botón (customId contiene authorId y targetId)
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
+
+    const embed = new EmbedBuilder()
+      .setColor("Pink")
+      .setDescription(`🤗 **${message.author.username}** ha abrazado a **${user.username}**!\n\n💞 Total abrazos: **${data[key]}**`)
+      .setImage(gif);
+
+    // customId: hugback_<authorId>_<targetId>
+    const customId = `hugback_${message.author.id}_${user.id}`;
+
+    const button = new ButtonBuilder()
+      .setCustomId(customId)
+      .setLabel("🤗 Abrazar de vuelta")
+      .setStyle(ButtonStyle.Primary);
+
+    const row = new ActionRowBuilder().addComponents(button);
+
+    await message.channel.send({ embeds: [embed], components: [row] });
+  }
 };
