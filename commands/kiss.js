ddd27f3fb4e05fe9e5062f3e64ef0bc1c136972f
duvzy <juanesjerez0101@gmail.com>
2026-03-13 02:16:48 -0500

    kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,58 +1,41 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
-const fs = require("fs");
-
-const gifs = [
-"https://media.tenor.com/GH7k9u9F8aQAAAAC/anime-kiss.gif",
-"https://media.tenor.com/4dL6dX6G8wUAAAAC/anime-kiss.gif",
-"https://media.tenor.com/VJ3gXq8dQxEAAAAC/kiss-anime.gif",
-"https://media.tenor.com/dJU8s0n8ZKAAAAAC/anime-kiss.gif",
-"https://media.tenor.com/1w8E9X8wXxAAAAAC/anime-kiss.gif"
-];
 
 module.exports = {
+
     name: "kiss",
+    description: "Besa a alguien 💋",
 
-    async execute(message, args, client) {
+    async execute(message) {
 
-        const user = message.mentions.users.first();
+        const target = message.mentions.users.first();
 
-        if (!user) {
-            return message.reply("💋 menciona a alguien para besar");
+        if (!target) {
+            return message.reply("Debes mencionar a alguien para besar 💋");
         }
 
-        if (user.id === message.author.id) {
-            return message.reply("😳 no puedes besarte a ti mismo");
+        if (target.bot) {
+            return message.reply("No puedes besar bots 🤖");
         }
 
-        const path = "./kisses.json";
-
-        let data = {};
-
-        if (fs.existsSync(path)) {
-            data = JSON.parse(fs.readFileSync(path, "utf8"));
+        if (target.id === message.author.id) {
+            return message.reply("No puedes besarte a ti mismo 💀");
         }
-
-        const key = `${message.author.id}_${user.id}`;
-
-        if (!data[key]) data[key] = 0;
-
-        data[key]++;
-
-        fs.writeFileSync(path, JSON.stringify(data, null, 2));
-
-        const gif = gifs[Math.floor(Math.random() * gifs.length)];
 
         const embed = new EmbedBuilder()
             .setColor("#ff4da6")
-            .setDescription(`💋 **${message.author.username}** besó a **${user.username}**!\n\n💕 Total besos: **${data[key]}**`)
-            .setImage(gif);
+            .setDescription(`💋 **${message.author.username}** quiere besar a **${target.username}**\n\n¿Aceptas el beso?`);
 
-        const button = new ButtonBuilder()
-            .setCustomId(`kissback_${message.author.id}_${user.id}`)
-            .setLabel("💋 Besar de vuelta")
-            .setStyle(ButtonStyle.Primary);
+        const accept = new ButtonBuilder()
+            .setCustomId(`kiss_accept_${message.author.id}_${target.id}`)
+            .setLabel("Aceptar 💋")
+            .setStyle(ButtonStyle.Success);
 
-        const row = new ActionRowBuilder().addComponents(button);
+        const reject = new ButtonBuilder()
+            .setCustomId(`kiss_reject_${message.author.id}_${target.id}`)
+            .setLabel("Rechazar ❌")
+            .setStyle(ButtonStyle.Danger);
+
+        const row = new ActionRowBuilder().addComponents(accept, reject);
 
         message.channel.send({
             embeds: [embed],
@@ -60,4 +43,5 @@ module.exports = {
         });
 
     }
+
 };
