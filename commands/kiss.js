7fc2614fc5eb70d3f9a115230aa01b1295d3a49d
duvzy <juanesjerez0101@gmail.com>
2026-03-13 16:31:10 -0500

    kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -2,46 +2,55 @@ const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("
 
 module.exports = {
 
-    name: "kiss",
-    description: "Besa a alguien 💋",
+name: "kiss",
+description: "Besa a alguien 💋",
 
-    async execute(message) {
+async execute(message) {
 
-        const target = message.mentions.users.first();
+const target = message.mentions.users.first();
 
-        if (!target) {
-            return message.reply("Debes mencionar a alguien para besar 💋");
-        }
+if (!target) {
+return message.reply("Debes mencionar a alguien para besar 💋");
+}
 
-        if (target.bot) {
-            return message.reply("No puedes besar bots 🤖");
-        }
+if (target.bot) {
+return message.reply("No puedes besar bots 🤖");
+}
 
-        if (target.id === message.author.id) {
-            return message.reply("No puedes besarte a ti mismo 💀");
-        }
+if (target.id === message.author.id) {
+return message.reply("No puedes besarte a ti mismo 💀");
+}
 
-        const embed = new EmbedBuilder()
-            .setColor("#ff4da6")
-            .setDescription(`💋 **${message.author.username}** quiere besar a **${target.username}**\n\n¿Aceptas el beso?`);
+const gifs = [
+"https://c.tenor.com/XUFt_n8RLlkAAAAC/tenor.gif",
+"https://c.tenor.com/oVfJLUT7X9IAAAAC/tenor.gif",
+"https://c.tenor.com/9u2vmryDP-cAAAAC/tenor.gif"
+];
 
-        const accept = new ButtonBuilder()
-            .setCustomId(`kiss_accept_${message.author.id}_${target.id}`)
-            .setLabel("Aceptar 💋")
-            .setStyle(ButtonStyle.Success);
+const gif = gifs[Math.floor(Math.random() * gifs.length)];
 
-        const reject = new ButtonBuilder()
-            .setCustomId(`kiss_reject_${message.author.id}_${target.id}`)
-            .setLabel("Rechazar ❌")
-            .setStyle(ButtonStyle.Danger);
+const embed = new EmbedBuilder()
+.setColor("#ff4da6")
+.setDescription(`💋 **${message.author.username}** quiere besar a **${target.username}**`)
+.setImage(gif);
 
-        const row = new ActionRowBuilder().addComponents(accept, reject);
+const accept = new ButtonBuilder()
+.setCustomId(`kiss_accept_${message.author.id}_${target.id}`)
+.setLabel("Aceptar 💋")
+.setStyle(ButtonStyle.Success);
 
-        message.channel.send({
-            embeds: [embed],
-            components: [row]
-        });
+const reject = new ButtonBuilder()
+.setCustomId(`kiss_reject_${message.author.id}_${target.id}`)
+.setLabel("Rechazar ❌")
+.setStyle(ButtonStyle.Danger);
 
-    }
+const row = new ActionRowBuilder().addComponents(accept, reject);
+
+message.channel.send({
+embeds: [embed],
+components: [row]
+});
+
+}
 
 };
