5af4d6435b0cfe60d6fe05ce21ffe4fd25456160
duvzy <juanesjerez0101@gmail.com>
2026-03-23 01:47:24 -0500

    Add files via upload

---
+++ commands/lock.js
@@ -0,0 +1,21 @@
+const { PermissionsBitField } = require("discord.js");
+
+module.exports = {
+    name: "lock",
+    description: "Bloquea el canal",
+
+    async execute(message) {
+
+        if (!message.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+            return message.reply("❌ No tienes permisos para usar esto");
+        }
+
+        const channel = message.channel;
+
+        await channel.permissionOverwrites.edit(message.guild.roles.everyone, {
+            SendMessages: false
+        });
+
+        message.reply("🔒 Canal bloqueado. Nadie puede escribir.");
+    }
+};
---
+++ commands/unlock.js
@@ -0,0 +1,21 @@
+const { PermissionsBitField } = require("discord.js");
+
+module.exports = {
+    name: "unlock",
+    description: "Desbloquea el canal",
+
+    async execute(message) {
+
+        if (!message.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+            return message.reply("❌ No tienes permisos para usar esto");
+        }
+
+        const channel = message.channel;
+
+        await channel.permissionOverwrites.edit(message.guild.roles.everyone, {
+            SendMessages: null
+        });
+
+        message.reply("🔓 Canal desbloqueado. Ya pueden escribir.");
+    }
+};
