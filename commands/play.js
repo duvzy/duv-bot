28d8e34b4024e181cef63a8f28790dbd8c563d49
duvzy <juanesjerez0101@gmail.com>
2026-03-26 06:06:32 -0500

    play.js

--- commands/play.js
+++ commands/play.js
@@ -1,32 +1,31 @@
 module.exports = {
     name: 'play',
     aliases: ['p'],
-    description: 'Reproduce una canción de YouTube o busca por nombre',
+    description: 'Reproduce música de YouTube',
 
     async execute(message, args, client) {
-        if (!message.member.voice.channel) {
-            return message.reply('❌ Debes estar en un **canal de voz** para poner música!');
+        const voiceChannel = message.member?.voice?.channel;
+        if (!voiceChannel) {
+            return message.reply('❌ Debes estar en un canal de voz para poner música!');
+        }
+
+        if (!args.length) {
+            return message.reply('❌ Escribe el nombre de la canción o un link de YouTube.\nEj: `.p despacito`');
         }
 
         const query = args.join(' ');
-        if (!query) {
-            return message.reply('❌ Escribe el nombre de la canción o el link.\nEjemplo: `.p despacito` o `.p https://youtu.be/...`');
-        }
 
         try {
-            // DisTube se encarga de todo: buscar + unirse al voice + reproducir
-            await client.distube.play(message.member.voice.channel, query, {
+            await client.distube.play(voiceChannel, query, {
                 member: message.member,
                 textChannel: message.channel,
                 message: message
             });
-
-            // El mensaje de "Añadido" o "Reproduciendo" lo maneja automáticamente 
-            // gracias a los eventos que ya tienes en el index.js
-
         } catch (error) {
-            console.error(error);
-            message.reply('❌ Error al intentar reproducir la música. Intenta con otro link o nombre.');
+            console.error("Error en play:", error);
+            if (message.channel) {
+                message.channel.send(`❌ Error: ${error.message || "No pude reproducir la canción"}`).catch(() => {});
+            }
         }
     }
 };
